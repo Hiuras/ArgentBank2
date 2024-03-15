54998c24f2b62b8a666ae2a9a46c1c8bdfea252b
Hiuras <137842225+Hiuras@users.noreply.github.com>
2024-03-15 15:24:57 +0100

    Upgrade Index

--- src/Components/Accueil.js
+++ src/Components/Accueil.js
@@ -4,60 +4,22 @@ import { useSelector } from "react-redux"; // Import the useSelector hook
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUserCircle, faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
 import Nav from "../Ressources/Nav/Nav";
+import Hero from "../Ressources/hero/Hero";
+import Features from "../Ressources/features/features";
 import logo from '../assets/argentBankLogo.png';
-import chat from '../assets/icon-chat.png'
-import money from '../assets/icon-money.png'
-import security from '../assets/icon-security.png'
 import Footer from "../Ressources/Footer/Footer";
 import '../Styles/Styles.css';
 
 function Accueil() {
-  const user = useSelector(state => state.user); // Access state from Redux store
+  const user = useSelector(state => state.user);
 
   return (
     <div>
       <div className="banner">
         <Nav />
         <main>
-          <div className="hero">
-            <section className="hero-content">
-              <h2 className="sr-only">Promoted content</h2>
-              <p className="subtitle">No fees.</p>
-              <p className="subtitle">No minimum deposit.</p>
-              <p className="subtitle">High interest rates.</p>
-              <p className="text">Open a savings account with Argent Bank today!</p>
-            </section>
-          </div>
-          <section className="features">
-            <h2 className="sr-only">Features</h2>
-            <div className="feature-item">
-              <img src={chat} alt="chat icon" className="feature-icon" />
-              <h3 className="feature-item-title">You are our #1 priority</h3>
-              <p>
-              Need to talk to a representative? You can get in touch through our
-            24/7 chat or through a phone call in less than 5 minutes.
-              </p>
-            </div>
-            <div className="feature-item">
-              <img src={money} alt="chat Icon" className="feature-icon" />
-              <h3 className="feature-item-title">
-              More savings means higher rates
-              </h3>
-              <p>
-                The more you save with us, the higher interest rate will be ! 
-              </p>
-            </div>
-            <div className="feature-item">
-              <img src={security} alt="chat icon" className="feature-icon"/>
-              <h3 className="featur-item-title">
-              Security you can trust
-              </h3>
-              <p>
-              We use top of the line encryption to make sure your data and money
-            is always safe.
-              </p>
-            </div>
-          </section>
+          <Hero /> 
+          <Features />
         </main>
         <Footer />
       </div>
